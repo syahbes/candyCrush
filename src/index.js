45ffd4e40025e7e9d7929accaf1fd52ec77b750f
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-06-25 15:49:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -11,5 +11,24 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+*/}
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      <h1>My First React Component!</h1>
+      </div>
+    );
+  }
+};
+ReactDOM.render(<MyComponent />, document.getElementById('root'))
 
 reportWebVitals(sendToVercelAnalytics);
