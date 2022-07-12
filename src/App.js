e7f8299345aa98856077e924c014635b6cf21cd2
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-07-12 21:28:41 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
-function App() {
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'red',
+  'yellow'
+]
+
+const App = () => {
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+  console.log(randomColorArrangement)
+  }
+
+createBoard()
+
   return (
     <div>
       <h1>nbnbnbnbnbnbnb!</h1>
