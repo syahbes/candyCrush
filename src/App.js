073294901c93aefd133e3d45b62b33812ee65801
Shlomi Yahbes <99732661+syahbes@users.noreply.github.com>
2022-07-15 06:53:15 +0300

    checkRowFour

--- src/App.js
+++ src/App.js
@@ -36,6 +36,36 @@ const cheakForColumsOfThree = () => {
   }
 }  
 
+const cheakForRowOfFour = () => {
+  for (let i = 0; i < 64; i++) {
+    const rowOfFour = [i, i + 1, i + 2, i +3]
+    const decidecColor = currentColorArrangment[i]
+    const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 61, 62, 63]
+
+    if (notValid.includes(i)) continue
+
+    if (rowOfFour.every(square => currentColorArrangment[square] === decidecColor)) {
+      rowOfFour.forEach(square => currentColorArrangment[square] = '')
+
+    }
+  }
+}  
+const cheakForRowOfThree = () => {
+  for (let i = 0; i < 64; i++) {
+    const rowOfThree = [i, i + 1, i + 2]
+    const decidecColor = currentColorArrangment[i]
+    const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63]
+
+    if (notValid.includes(i)) continue
+
+    if (rowOfThree.every(square => currentColorArrangment[square] === decidecColor)) {
+      rowOfThree.forEach(square => currentColorArrangment[square] = '')
+
+    }
+  }
+}  
+
+
 const createBoard = () => {
     const randomColorArrangement = []
     for (let i = 0; i < width * width; i++) {
@@ -53,11 +83,14 @@ const createBoard = () => {
     const timer = setInterval(() => {
       cheakForColumsOfFour()
       cheakForColumsOfThree()
+
+      cheakForRowOfFour
+      cheakForRowOfThree()
       setCurrentColorArrangment([...currentColorArrangment])
     }, 100)
     return () => clearInterval(timer)
 
-   }, [cheakForColumsOfFour,cheakForColumsOfThree, currentColorArrangment])
+   }, [cheakForColumsOfFour, cheakForColumsOfThree, cheakForRowOfFour, cheakForRowOfThree, currentColorArrangment])
 
 
   return (
